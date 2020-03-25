e5094b423ff2ac377f3c140192e71dc1dc1fece6
Tania <tania.rojas@edu.itescia.fr>
2020-03-25 09:13:43 +0100

    jouer un son

--- components/Game.js
+++ components/Game.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { Text, FlatList, View, StyleSheet } from 'react-native';
+import { Text, FlatList, View, StyleSheet, TouchableOpacity  } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { Audio } from 'expo-av';
+
 
 
 
 export default class Game extends React.Component {
+
+     async componentDidMount (){
+         this.sound = {};
+         this.sound.click = new Audio.Sound();
+         try {
+         await this.sound.click.loadAsync( require ('../assets/sound/amelie_poulain.mp3') );
+         } catch (error) {
+         console.log('errorSound', error);
+         }
+     };
+     _onPress() {
+     this.sound.click.replayAsync();
+     };
+
 
 render(){
     return (
@@ -15,6 +31,11 @@ render(){
         <View style={{width: 300, height: 300, backgroundColor: 'chartreuse'}} />
         <View style={{width: 300, height: 300, backgroundColor: 'crimson'}} />
         <View style={{width: 300, height: 300, backgroundColor: 'gold'}} />
+        <TouchableOpacity onPress= {() =>this._onPress()}>
+            <View>
+                <Text>Start</Text>
+            </View>
+        </TouchableOpacity>
       </View>
     );
   }
