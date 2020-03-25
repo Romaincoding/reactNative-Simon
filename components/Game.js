f27b1942e84289531354adfd8b0b95cf0e85897f
Romain <romain.dupuis@edu.itescia.fr>
2020-03-25 13:24:00 +0100

    Ia choix ok

--- components/Game.js
+++ components/Game.js
@@ -14,40 +14,65 @@ export default class Game extends React.Component {
    constructor(props) {
       super(props);
       this.state = {
+
             Simon : {
-             bleu : { style : styles.TuileBleu},
-             rouge : { style :styles.TuileRouge},
-             jaune : { style : styles.TuileJaune},
-             vert : { style : styles.TuileVert}
-          }
+             bleu : {
+
+             style : 'TuileBleu'},
+             rouge : {
+
+              style :'TuileRouge'},
+             jaune : {
+
+             style : 'TuileJaune'},
+             vert : {
+
+             style : 'TuileVert'}
+          },
+
+          sequenceIa : [],
+
 
 
       }
 
-      { opacity: 1 }
+
    }
 
  selectionCouleur = () =>{
+ console.log("fonction ok");
      var couleur = couleurTab[Math.floor(Math.random() * 4)]
 
      switch (couleur){
 
          case "jaune":
-            return jaune.style.height = 150;
+          console.log("l'état est " + this.state);
+        this.setState({...this.state, Simon: {...this.state.Simon, jaune: {...this.state.Simon.jaune, style: 'TuileJauneLight'}}});
+         console.log("le nouvel état  est " + this.state);
+        break;
 
-          console.log("la couleur 5 est " + couleur);
+
+
+
 
          case "bleu":
-         console.log("la couleur est " + couleur);
-            return bleu.style.height = 150;
-
+         console.log("l'état est " + this.state);
+      this.setState({...this.state, Simon: {...this.state.Simon, bleu: {...this.state.Simon.bleu, style: 'TuileBleuLight'}}});
+        console.log("le nouvel état  est " + this.state);
+      break;
          case "rouge":
-            return rouge.style.height = 150;
+          console.log("l'état est " + this.state);
+      this.setState({...this.state, Simon: {...this.state.Simon, rouge: {...this.state.Simon.rouge, style: 'TuileRougeLight'}}});
+        console.log("le nouvel état  est " + this.state);
+      break;
 
          case "vert":
-            return  vert.style.height = 150;
+          console.log("l'état est " + this.state);
+       this.setState( {...this.state, Simon: {...this.state.Simon, vert: {...this.state.Simon.vert, style: 'TuileVertLight'}}});
+         console.log("le nouvel état  est " + this.state);
+         break;//    this.setState({...this.state, Simon.vert.style: opacity: 1});
 
-          console.log("la couleur 4 est " + couleur);
+         // console.log("la couleur 4 est " + couleur);
 
       };
 
@@ -84,13 +109,15 @@ export default class Game extends React.Component {
             </View>
 
            <View style={styles.RangeeTuiles}>
-             <TouchableOpacity style={this.state.Simon.bleu.style} activeOpacity={0.6} onPress= {() =>this._onPress(this.sound.clickBleu)}/>
-             <TouchableOpacity  style={this.state.Simon.vert.style} activeOpacity={0.6} onPress= {() =>this._onPress(this.sound.clickVert)}/>
+
+             <TouchableOpacity style={styles[this.state.Simon.bleu.style]} activeOpacity={0.6} onPress= {() =>this._onPress(this.sound.clickBleu)}/>
+             <TouchableOpacity  style={styles[this.state.Simon.vert.style]} activeOpacity={0.6} onPress= {() =>this._onPress(this.sound.clickVert)}/>
            </View>
 
            <View style={styles.RangeeTuiles}>
-             <TouchableOpacity style={this.state.Simon.rouge.style} activeOpacity={0.6} onPress= {() =>this._onPress(this.sound.clickRouge)}/>
-             <TouchableOpacity style={this.state.Simon.jaune.style} activeOpacity={0.6} onPress= {() =>this._onPress(this.sound.clickJaune)}/>
+             <TouchableOpacity style={styles[this.state.Simon.rouge.style]} activeOpacity={0.6} onPress= {() =>this._onPress(this.sound.clickRouge)}/>
+             <TouchableOpacity style={styles[this.state.Simon.jaune.style]} activeOpacity={0.6} onPress= {() =>this._onPress(this.sound.clickJaune)}/>
+
            </View>
 
            <View style={styles.RangeeTuiles}>
@@ -98,6 +125,14 @@ export default class Game extends React.Component {
                  <Text style={styles.textBouton}>Jouer</Text>
              </TouchableOpacity>
             </View>
+
+
+            <TouchableOpacity onPress= {() =>this.selectionCouleur()}>
+                <View>
+                    <Text>Start</Text>
+                </View>
+            </TouchableOpacity>
+
        </View>
      );
 
@@ -121,25 +156,62 @@ export default class Game extends React.Component {
                  width: 100,
                  height: 100,
                  backgroundColor: "#01579b",
-                 opacity: 0.7
+                 opacity: 1
+
              },
+              TuileBleuLight : {
+                              width: 100,
+                              height: 100,
+                              backgroundColor: "#01579b",
+                              opacity: 0.5
+
+                          },
          TuileVert : {
                    width: 100,
                    height: 100,
                    backgroundColor: '#1b5e20',
+                   opacity: 1
+
                },
+                  TuileVertLight : {
+                                  width: 100,
+                                  height: 100,
+                                  backgroundColor: '#1b5e20',
+                                  opacity: 0.5
+
+                              },
          TuileRouge : {
                  width: 100,
                  height: 100,
                  backgroundColor: '#bf360c',
-                 marginTop: 25
+                 marginTop: 25,
+                 opacity: 1
+
              },
+                 TuileRougeLight : {
+                              width: 100,
+                              height: 100,
+                              backgroundColor: '#bf360c',
+                              marginTop: 25,
+                              opacity: 0.5
+
+                          },
          TuileJaune : {
                    width: 100,
                    height: 100,
                    backgroundColor: '#fbc02d',
-                   marginTop: 25
+                   marginTop: 25,
+                   opacity: 1
+
                },
+                 TuileJauneLight : {
+                                  width: 100,
+                                  height: 100,
+                                  backgroundColor: '#fbc02d',
+                                  marginTop: 25,
+                                  opacity: 0.5
+
+                              },
          BoutonJouer : {
                     width: 100,
                     backgroundColor: '#424242',
