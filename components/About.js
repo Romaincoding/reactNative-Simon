5543024e0074741ec74d54ae4f15ac6b59fb5cbb
Tania <tania.rojas@edu.itescia.fr>
2020-03-25 11:44:00 +0100

    team

--- components/About.js
+++ components/About.js
@@ -2,49 +2,41 @@ import React from 'react';
 import { Text, FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { connect } from 'react-redux'
-
-
-
 import styles from '../assets/styles';
 
 class About extends React.Component {
 
-
 	render() {
-	console.log("coco" + this.props.pseudo)
-
+	const { team } = this.props;
+	console.log("coco" + this.props.prenom)
 
 		return (
 			<SafeAreaView style={styles.about}>
 
-				<Text>{this.props.profil.map(y => y.pseudo)}</Text>
-
-
-
-
-
+				<Text>{this.props.team.map(y => y.prenom)}</Text>
 
 			</SafeAreaView>
 		);
 	}
 }
+
 // je récupère un state que je récupère via redux en props de mon composant
 const mapStateToProps = state => {
 	return {
-		profil: state.profil,
+		team: state.team,
 		adresse: state.adresse
 
 	};
 }
-const mapDispatchToProps = dispatch => {
+/*const mapDispatchToProps = dispatch => {
 	return {
-		editProfil: profil => {
-			dispatch(editProfil(profil))
+		editTeam: team => {
+			dispatch(editTeam(team))
 		}
 	};
-}
+}*/
 
 export default connect(
-	mapStateToProps,
-	mapDispatchToProps
+	mapStateToProps
+//	mapDispatchToProps
 )(About);
--- redux/reducer.js
+++ redux/reducer.js
@@ -11,46 +11,45 @@ const initialState = {
 
         joueur:"Michel ",
         scores: "3 "
-
     },
     {
-            joueur:"Romain ",
-            scores:"5 "
+        joueur:"Romain ",
+        scores:"5 "
+    },
+    {
+        joueur:"Hugo ",
+        scores: "7 "
+    },
+    {
+        joueur:"Bertand ",
+        scores: "8 "
+    }],
 
-        },
-        {
-                 joueur:"Hugo ",
-                scores: "7 "
-
-            },
-            {
-                 joueur:"Bertand ",
-                    scores: "8 "
-
-                }],
-    // state.profil
- 	profil: [ {
-
- 		pseudo: 'Romain ',
+    // state.team
+ 	team: [ {
+ 		prenom: 'Romain ',
  		age: '35ans'
-
- 	}, {
- 	pseudo: 'Boris ',
-     		age: '37ans'
-
  	},
  	{
-     	pseudo: 'Hugo ',
-         		age: '37ans'
+ 	    prenom: 'Boris ',
+     	age: '37ans'
+ 	},
+ 	{
+     	prenom: 'Hugo ',
+        age: '37ans'
+    },
+    {
+        prenom: 'Teddy ',
+        age: '37ans'
 
-     	},
-     	{
-         	pseudo: 'Teddy ',
-             		age: '37ans'
+    }],
 
-         	}]
+    // state.profil
+    profil: {
+    pseudo:''
+    }
 
- 	}
+ }
 
 
 export default function reducer(state = initialState, action) {
